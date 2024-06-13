3dd14ebe5b714492137f55a381223b9c9f263a9e
Navadeepak.C <128801710+navadeepak@users.noreply.github.com>
2024-06-13 18:19:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,268 @@
+import './App.css';
+import './index.js';
+
+function App() {
+  const chng=()=>{
+    document.getElementById("payment-id").style="display:none;";
+    document.getElementById("nav").style="display:flex;";
+    document.getElementById("content").style="display:grid;";
+    document.getElementById("address").value="";
+    document.getElementById("quantity").value="";
+  };
+  const b1=()=>{
+    let n=document.querySelector(".but1").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹70";
+  }
+  const b2=()=>{
+    let n=document.querySelector(".but2").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹150";
+  }
+  const b3=()=>{
+    let n=document.querySelector(".but3").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹100";
+  }
+  const b4=()=>{
+    let n=document.querySelector(".but4").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹200";
+  }
+  const b5=()=>{
+    let n=document.querySelector(".but5").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹150";
+  }
+  const b6=()=>{
+    let n=document.querySelector(".but6").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹300";
+  }
+  const b7=()=>{
+    let n=document.querySelector(".but7").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹100";
+  }
+  const b8=()=>{
+    let n=document.querySelector(".but8").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹150";
+  }
+  const b9=()=>{
+    let n=document.querySelector(".but9").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹200";
+  }
+  const b10=()=>{
+    let n=document.querySelector(".but10").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹200";
+  }
+  const b11=()=>{
+    let n=document.querySelector(".but11").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹200";
+  }
+  const b12=()=>{
+    let n=document.querySelector(".but12").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹200";
+  }
+  const b13=()=>{
+    let n=document.querySelector(".but13").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹200";
+  }
+  const b14=()=>{
+    let n=document.querySelector(".but14").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹150";
+  }
+  const b15=()=>{
+    let n=document.querySelector(".but15").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹150";
+  }
+  const b16=()=>{
+    let n=document.querySelector(".but16").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹125";
+  }
+  const b17=()=>{
+    let n=document.querySelector(".but17").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹125";
+  }
+  const b18=()=>{
+    let n=document.querySelector(".but18").value;
+    document.getElementById("payment-id").style="display:flex;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:none;";
+    document.getElementById("title-order").innerHTML=n;
+    document.getElementById("price-order").innerHTML="₹125";
+  }
+  const clickeve=()=>{
+    // eslint-disable-next-line no-mixed-operators
+    if ( document.getElementById("address").value==="" || document.getElementById("quantity").value==="" || document.getElementById("quantity").value>20 ){
+      alert("please fill address and quantity, and payment method or only minimum 20 burgers only order on one time!");
+      document.getElementById("payment-id").style="display:flex;";
+      document.getElementById("nav").style="display:none;";
+      document.getElementById("content").style="display:none;";
+    }
+    else{
+      alert("Thank You For Order!");
+      document.getElementById("payment-id").style="display:none;";
+      document.getElementById("nav").style="display:flex;";
+      document.getElementById("content").style="display:grid;";
+      document.getElementById("address").value="";
+    document.getElementById("quantity").value="";
+    }
+  };
+  const con=()=>{
+    document.getElementById("payment-id").style="display:none;";
+    document.getElementById("nav").style="display:none;";
+    document.getElementById("content").style="display:none;";
+    document.getElementById("conts").style="display:flex";
+  }
+  const bk=()=>{
+    document.getElementById("payment-id").style="display:none;";
+    document.getElementById("nav").style="display:flex;";
+    document.getElementById("content").style="display:grid;";
+    document.getElementById("conts").style="display:none";
+  }
+  return (
+    <div className="App">
+      <div id='nav' className='nav'>
+        <p className='title'>Big Burgggger</p>
+        <div className='nav-list'>
+          <p className='contact' onClick={con}>Contact</p>
+          <p className='help' onClick={()=>{alert("This is Burger order web page this is sample UI Design for a developer skill improvement. Created by : Navadeepak");}}>Help</p>
+        </div>
+      </div>
+      <div id='content' className='content'>
+        <div className='box'>Hamburger<span className='price'>₹70</span><button onClick={b1} value="Hamburger" className='but but1' id='Hamburger'>Order</button></div>
+        <div className='box'>Turkey Burger<span className='price'>₹150</span><button onClick={b2} value="Turkey Burger" className='but but2' id='Turkey '>Order</button></div>
+        <div className='box'>Classic Burger<span className='price'>₹100</span><button onClick={b3} value="Classic Burger" className='but but3' id='Classic '>Order</button></div>
+        <div className='box'>Lamb Burger<span className='price'>₹200</span><button onClick={b4} value="Lamb Burger" className='but but4' id='Lamb '>Order</button></div>
+        <div className='box'>Chicken Burger<span className='price'>₹150</span><button onClick={b5} value="Chicken Burger" className='but but5' id='Chicken '>Order</button></div>
+        <div className='box'>Specialty Burger<span className='price'>₹300</span><button onClick={b6} value="Specialty Burger" className='but but6' id='Specialty '>Order</button></div>
+        <div className='box'>Cheese Burger<span className='price'>₹100</span><button onClick={b7} value="Cheese Burger" className='but but7' id='Cheese '>Order</button></div>
+        <div className='box'>Bacon Burger<span className='price'>₹150</span><button onClick={b8} value="Bacon Burger" className='but but8' id='Bacon '>Order</button></div>
+        <div className='box'>Veggie Burger<span className='price'>₹200</span><button onClick={b9} value="Veggie Burger" className='but but9' id='Veggie '>Order</button></div>
+        <div className='box'>Shawarma Burger<span className='price'>₹200</span><button onClick={b10} value="Shawarma Burger" className='but but10' id='Shawarma '>Order</button></div>
+        <div className='box'>Muffaletta Burger<span className='price'>₹200</span><button onClick={b11} value="Muffaletta Burger" className='but but11' id='Muffaletta '>Order</button></div>
+        <div className='box'>Kimchi Burger<span className='price'>₹200</span><button onClick={b12} value="Kimchi Burger" className='but but12' id='Kimchi '>Order</button></div>
+        <div className='box'>Kubie Burger<span className='price'>₹200</span><button onClick={b13} value="Kubie Burger" className='but but13' id='Kubie '>Order</button></div>
+        <div className='box'>Nutburger<span className='price'>₹150</span><button onClick={b14} value="Nutburger" className='but but14' id='Nutburger'>Order</button></div>
+        <div className='box'>Olive Burger<span className='price'>₹150</span><button onClick={b15} value="Olive Burger" className='but but15' id='Olive '>Order</button></div>
+        <div className='box'>Pimento Cheeseburger<span className='price'>₹125</span><button onClick={b16} value="Pimento Cheeseburger" className='but but16' id='Pimento '>Order</button></div>
+        <div className='box'>Green Chile Cheeseburger<span className='price'>₹125</span><button onClick={b17} value="Green Chile Cheeseburger" className='but but17' id='GreenChile'>Order</button></div>
+        <div className='box'>Slaw Burger<span className='price'>₹125</span><button onClick={b18} value="Slaw Burger" className='but but18' id='Slaw '>Order</button></div>
+      </div>
+      <div id='payment-id' className='payment'>
+        <p className='title'>Big Burgggger</p>
+        <p id='title-order'>Name</p>
+        <p id='price-order'>Price</p>
+        <div className='address'>
+          <label>Address:</label>
+          <textarea id='address' placeholder='Address' className='inp' rows="4" cols="50"></textarea>
+        </div>
+        <div className='qnt'>
+          <label>Quantity:</label>
+          <input id='quantity' placeholder='Quantity' className='inp' type='number'></input>
+        </div>
+        <div className='pay'>
+          <label>Payment Method:</label>
+          <select className='slt'>
+            <option>Gpay</option>
+            <option>Paytm</option>
+            <option>Amazon Pay</option>
+            <option>PhonePe</option>
+            <option>Net Banking</option>
+            <option>Cash on Delivery</option>
+          </select>
+        </div>
+        <button id='order-conform' className='inp ord' onClick={clickeve}>Order</button>
+        <button id="back-to-purches" className='inp bak' onClick={chng}>Back</button>
+      </div>
+      <div className='contact-main' id='conts'>
+        <a href="https://wa.me/917603857110">Whatsapp</a>
+        <a href="mailto:navadeepakaswin007@gmail.com">Mail</a>
+        <a href="tel:+917603857110">Call</a>
+        <p className='bk-cont' onClick={bk}>Back</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
